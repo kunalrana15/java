92624536d993d32295cb2a858b1ccdfc643c456f
Kunal Rana <111565729+kunalrana15@users.noreply.github.com>
2026-03-30 16:27:34 +0000

    Learning Datatypes in Java

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+        public static void main(String[] args) {
+            boolean isJavaFun = true;
+            boolean isFishTasty = false;
+            int x=5;
+            double y=5.2;
+            long worldPopulation = 7800000000L;
+            System.out.println("Is Java fun? " + isJavaFun);
+            System.out.println("Is fish tasty? " + isFishTasty);
+            System.out.println("Value of x is:" + x);
+            System.out.println("Value of y is:" + y);
+        }
+}
