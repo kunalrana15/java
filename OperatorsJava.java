caf5a8b313e5f27dfaf28b0ce8df8657aec54d2f
Kunal Rana <111565729+kunalrana15@users.noreply.github.com>
2026-04-02 16:07:29 +0000

    Learning operators in Java

---
+++ OperatorsJava.java
@@ -0,0 +1,16 @@
+public class OperatorsJava{
+    public static void main(String[] args){
+        int a=25;
+        int b=20;
+
+        int sum=a+b;
+        int sub=a-b;
+        int multi=a*b;
+        int div=a/b;
+
+        System.out.println("Addition is:"+sum);
+        System.out.println("Substraction is:"+sub);
+        System.out.println("Multiplication is:"+multi);
+        System.out.println("Division is:"+div);
+    }
+}
