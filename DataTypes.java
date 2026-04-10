a2a1fda959b87ba7a29a21525159365b7569b39c
Kunal Rana <111565729+kunalrana15@users.noreply.github.com>
2026-04-10 15:04:42 +0000

    Fix merge conflicts

--- DataTypes.java
+++ DataTypes.java
@@ -11,3 +11,26 @@ public class DataTypes {
             System.out.println("Value of y is:" + y);
         }
 }
+public class Geeks{
+        
+            public static void main(String[] args){
+                    
+                            // Primitive array
+                                    int[] arr = {10, 20, 30, 40};
+                                            int n = arr.length;
+
+                                                    System.out.print("Primitive Array -> ");
+                                                            for (int i = 0; i < n; i++)
+                                                                        System.out.print(arr[i] + " ");
+
+                                                                                System.out.println();
+
+                                                                                        // Non-primitive array (String objects)
+                                                                                                String[] names = {"Lakshit", "Rahul", "Pankaj"};
+
+                                                                                                        System.out.print("Non-Primitive Array -> ");
+                                                                                                                for (int i = 0; i < names.length; i++)
+                                                                                                                            System.out.print(names[i] + " ");
+                                                                                                                                }
+                                                                                                                                }
+}
